bd8cb35f840a0c8079e5537300baf9f2307d29b2
sriram <sriramlokesh7811@gmail.com>
2025-07-15 11:05:35 +0530

    WIP: local changes before pulling from origin

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import './App.css';
-import Login from './Components/Login/Login1';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login1 from './Components/Login/Login1';
+import Dashboard from './Components/Dashboard/Dashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login1 />} />
+        <Route path="/Login1" element={<Login1 />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
+// import React from 'react';
+
+// import './App.css';
+// import Login from './Components/Login/Login1';
+// import Dashboard from './Components/Dashboard/Dashboard';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Dashboard />
+//     </div>
+//   );
+// }
+
+// export default App;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import Login from './Components/Login/Login1';
-import Login1 from './Components/Login/Login1'
+// import Login1 from './Components/Login/Login1'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login1 />
+    <App />
   </React.StrictMode>
 );
 
